2f3ae6b235db9f256aa66543db36be2b9e9924ff
myyandar <myyandar@hotmail.com>
2025-12-17 18:27:20 +0000

    Worked on AI provider adapters, streamlining types

--- AIArbitration.API/Program.cs
+++ AIArbitration.API/Program.cs
@@ -1,8 +1,13 @@
+using AIArbitration.API.Middleware;
+using AIArbitration.Core;
+using AIArbitration.Infrastructure.Interfaces;
+using AIArbitration.Infrastructure.Services;
+using AIArbitration.Infrastructure.ServiceSupport;
+using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 using System.Threading.RateLimiting;
 using UAParser.Extensions;
 using UAParser.Interfaces;
-using AIArbitration.Infrastructure.Interfaces;
 using AIArbitration.Infrastructure.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,10 +17,22 @@ builder.AddServiceDefaults();
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add memory cache
+builder.Services.AddMemoryCache();
+
+// Add database context
+builder.Services.AddDbContext<AIArbitrationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddUserAgentParser();
+builder.Services.AddProviderConfigurations(builder.Configuration);
+builder.Services.AddProviderAdapters();
+builder.Services.AddHttpClient();
 
 // using StackExchange.Redis;
 var redis = ConnectionMultiplexer.Connect("localhost:6379"); // use config from settings
@@ -30,11 +47,15 @@ app.MapDefaultEndpoints();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+// Add exception handling middleware
+app.UseAIArbitrationExceptionHandler();
 
 app.MapControllers();
 
